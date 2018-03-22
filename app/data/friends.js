6991a2b2915cedc2cb12f294be6bbfe884a8948f
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-03-21 17:08:31 -0700

    working on returing best friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,12 +3,12 @@ var peopleArray = [
     {
         name: "Fredrik", 
         photo: "imglink", 
-        score: ["1","2","3","4","5","1","2","3","4"]
+        score: [1,2,3,4,5,6,7,8,9]
     }, 
     {
         name: "Fredrik", 
         photo: "imglink", 
-        score: ["1","2","3","1","5","1","2","3","4"]
+        score: [1,2,3,4,5,6,5,8,9]
     }, 
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,22 +7,42 @@ module.exports = function (app) {
     }); 
 
     app.post("/api/friends", function (req, res){
-        var data = req.body
-        //get the new persons array of score and convert into int.
-        var newPersonScore = data.score.map(Number);
-        //function for adding two nnumbers
-        add = (a, b) =>
-        a + b
-        newPersonScore = newPersonScore.reduce(add)
-        console.log(newPersonScore); 
-        peopleArray.push(data);
-        for (var i = 0; i < peopleArray.length - 1; i++){
-            var totScore = peopleArray[i].score.map(Number);
-            totScore = totScore.reduce(add);
-            console.log(totScore); 
+        var newPersonScore = req.body.score.map(Number); 
+        var highScore = 999; 
+        var newFriend 
+        console.log(peopleArray[0].score); 
+        for (var i = 0; peopleArray.length; i++ ){
+            var sum = 0; 
+            for (j = 0; j < peopleArray[i].score.length; j++) {
+                sum += Math.abs(newPersonScore[j] - peopleArray[i].score[j]);
+            }
+            if (sum < highScore) {
+                highScore = sum;
+                newFriend = peopleArray[i];
+            }
         }
        
-        res.end(); 
+        //var peopleArrayScoreInt = []; 
         
+        //peopleArray.push(req.body);
+        /*for (var i = 0; i < peopleArray.length; i++){
+          peopleArrayScoreInt.push(peopleArray[i].score.map(Number))
+        }
+
+        var highScore = 999; 
+        var newFriend 
+        for (var j =0; peopleArrayScoreInt.length; j++){
+            var sum = 0; 
+            for (var k = 0; k < peopleArrayScoreInt[j].length; k++) {
+                sum += Math.abs(newPersonScore[k] - peopleArrayScoreInt[k]);
+            }
+            if (sum < highScore) {
+                highScore = sum; 
+                newFriend = peopleArray[i]
+            }
+
+        }*/
+
+        res.send(newFriend); 
     });
 }
